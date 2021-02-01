2a3dfb1218e8742108851709ad7ecfac8392b51d
Michael Griffith <griffithcmichael@gmail.com>
2021-02-01 13:00:38 -0600

    screen files set up

---
+++ src/screens/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import styles from './sytles';
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text>Some Text</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
