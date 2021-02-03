2366d0000d587319a8211e1d6fd4575fa74ac88d
Michael Griffith <griffithcmichael@gmail.com>
2021-02-03 13:52:39 -0600

    questions page

--- App.js
+++ App.js
@@ -8,11 +8,12 @@
 
 import React from 'react';
 import {SafeAreaView, StatusBar} from 'react-native';
-import DestinationSearch from './src/screens/DestinationSearch';
+// import DestinationSearch from './src/screens/DestinationSearch';
 // import HomeScreen from './src/screens/Home';
 // import Post from './src/components/Post';
 // import feed from './assets/data/feed';
 // import SearchResultsScreen from './src/screens/SearchResults';
+import HoursScreen from './src/screens/Hours';
 
 // const post1 = feed[0];
 
@@ -24,7 +25,8 @@ const App: () => React$Node = () => {
         {/* <HomeScreen /> */}
         {/* <Post post={post1} /> */}
         {/* <SearchResultsScreen /> */}
-        <DestinationSearch />
+        {/* <DestinationSearch /> */}
+        <HoursScreen />
       </SafeAreaView>
     </>
   );
---
+++ src/screens/Hours/index.js
@@ -0,0 +1,59 @@
+/* eslint-disable react-native/no-inline-styles */
+import React, {useState} from 'react';
+import {View, Text, Pressable} from 'react-native';
+import styles from './styles';
+
+const HoursScreen = () => {
+  const [hours, setHours] = useState(2);
+  const [guests, setGuests] = useState(10);
+
+  return (
+    <View style={{justifyContent: 'space-between', height: '100%'}}>
+      <View>
+        <View style={styles.row}>
+          <View>
+            <Text style={{fontWeight: 'bold'}}>Hours Needed</Text>
+            <Text style={{color: '#8d8d8d'}}>Minimum 2 Hours</Text>
+          </View>
+          <View style={{flexDirection: 'row', alignItems: 'center'}}>
+            <Pressable
+              onPress={() => setHours(Math.max(2, hours - 1))}
+              style={styles.button}>
+              <Text style={{fontSize: 20, color: '#474747'}}>-</Text>
+            </Pressable>
+            <Text style={{marginHorizontal: 20, fontSize: 16}}>{hours}</Text>
+            <Pressable
+              onPress={() => setHours(hours + 1)}
+              style={styles.button}>
+              <Text style={{fontSize: 20, color: '#474747'}}>+</Text>
+            </Pressable>
+          </View>
+        </View>
+        <View style={styles.row}>
+          <View>
+            <Text style={{fontWeight: 'bold'}}>Number of Guest</Text>
+            <Text style={{color: '#8d8d8d'}}>
+              The number is not to exceed total 
+            </Text>
+            <Text style={{color: '#8d8d8d'}}>Allows for need of assitant</Text>
+          </View>
+          <View style={{flexDirection: 'row', alignItems: 'center'}}>
+            <Pressable
+              onPress={() => setGuests(Math.max(10, guests - 10))}
+              style={styles.button}>
+              <Text style={{fontSize: 20, color: '#474747'}}>-</Text>
+            </Pressable>
+            <Text style={{marginHorizontal: 20, fontSize: 16}}>{guests}</Text>
+            <Pressable
+              onPress={() => setGuests(guests + 10)}
+              style={styles.button}>
+              <Text style={{fontSize: 20, color: '#474747'}}>+</Text>
+            </Pressable>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default HoursScreen;
