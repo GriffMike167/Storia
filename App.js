ba9ea4b4b7d10a8c6e39495317538a8855f08582
Michael Griffith <griffithcmichael@gmail.com>
2021-02-09 18:04:47 -0600

    moving files

--- App.js
+++ App.js
@@ -9,9 +9,7 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import {SafeAreaView, StatusBar} from 'react-native';
 import Router from './src/navigation/Router';
-import Amplify from 'aws-amplify'
-import config from './aws-exports'
-Amplify.configure(config)
+
 
 // const post1 = feed[0];
 
