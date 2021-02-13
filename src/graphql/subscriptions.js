014a2c48f5efccef1c9e0957cf998c0c303e7bef
Michael Griffith <griffithcmichael@gmail.com>
2021-02-12 18:24:51 -0600

    schema set up

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,63 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreatePost = /* GraphQL */ `
+  subscription OnCreatePost {
+    onCreatePost {
+      id
+      image
+      photographer
+      description
+      currentPrice
+      specialPrice
+      equipment
+      props
+      workExperince
+      venuesWorked
+      latitude
+      longitude
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdatePost = /* GraphQL */ `
+  subscription OnUpdatePost {
+    onUpdatePost {
+      id
+      image
+      photographer
+      description
+      currentPrice
+      specialPrice
+      equipment
+      props
+      workExperince
+      venuesWorked
+      latitude
+      longitude
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeletePost = /* GraphQL */ `
+  subscription OnDeletePost {
+    onDeletePost {
+      id
+      image
+      photographer
+      description
+      currentPrice
+      specialPrice
+      equipment
+      props
+      workExperince
+      venuesWorked
+      latitude
+      longitude
+      createdAt
+      updatedAt
+    }
+  }
+`;
