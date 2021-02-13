94675638b97f1e7c1671f5bf9306063fc40c9e56
Michael Griffith <griffithcmichael@gmail.com>
2021-02-12 23:54:28 -0600

    fixed errors in dummy data

--- assets/data/venues.js
+++ assets/data/venues.js
@@ -1,141 +1,141 @@
 export default [
-    {
-      id: "0",
-      image: "/Users/Michael/repos/storia/assets/images/21.jpg",
-      venue: "The Roof Barn",
-      description:
-        "The Tin Roof Barn is more than 100 years old and the venue’s website is refreshingly simple with beautiful illustrations. You won’t find any fancy features and virtual tools here, but rather hand-drawn doodles and a stunning hand drawn map of the area.",
-      specialPrice: 2050,
-      // specialPrice: 1005,
-      totalPrice: 4100,
-      coordinate: {
-        latitude: 32.6428463,
-        longitude: -96.9283827,
-      },
-    },
-    {
-        id: "1",
-        image: "/Users/Michael/repos/storia/assets/images/22.jpg",
-        venue: "Sage Gateshead",
-         description:
-        "Primarily a music venue, Sage Gateshead takes advantage of its central location and large armadillo structure to host events and meetings, as well as shows and concerts. The description of each event space is clearly detailed, complete with virtual tours powered by Google and presentation videos.",
-         specialPrice: 2250,
-        //  specialPrice: 1105,
-         totalPrice: 4500,
-         coordinate: {
-        latitude: 31.7428463,
-        longitude: -96.9283827,
-         },
-      },
-      {
-        id: "2",
-        image: "/Users/Michael/repos/storia/assets/images/23.png",
-        venue: "The Egg",
-        description:
-        "The Egg is a 5,000 sqm congress and meeting center located in the heart of Brussels. Their website clearly shows the potential of the venue with lots of pictures showing how versatile the spaces are. The document zone offers essential information event planners should have at hand when planning an event at the Egg,",
-        specialPrice: 2010,
-        //  specialPrice: 905,
-         totalPrice: 4020,
-         coordinate: {
-        latitude: 33.6428463,
-        longitude: -96.9283827,
-         },
-      },
-      {
-        id: "3",
-        image: "/Users/Michael/repos/storia/assets/images/24.jpg",
-        venue: "Bespoke",
-        description:
-            "Bespoke is a recently established event venue in San Francisco and is, in its own words, “tailored for tech”. Featuring a co-working space, a demo zone for pop-ups and ups and an event venue, the website is minimalist but interactive: the virtual tour of the space is seamless, and options and services are clearly labelled and priced",
-        specialPrice: 2250,
-        // specialPrice: 1105,
-        totalPrice: 5000,
-        coordinate: {
-        latitude: 32.6428463,
-        longitude: -97.9283827,
-        },
-      },
-]
-
-mutation createVenue1 {
-  createPost(input: {
-    image: "/Users/Michael/repos/storia/assets/images/21.jpg",
-    venue: "The Roof Barn",
+  {
+    id: '0',
+    image: '/Users/Michael/repos/storia/assets/images/21.jpg',
+    venue: 'The Roof Barn',
     description:
-      "The Tin Roof Barn is more than 100 years old and the venue’s website is refreshingly simple with beautiful illustrations. You won’t find any fancy features and virtual tools here, but rather hand-drawn doodles and a stunning hand drawn map of the area.",
+      'The Tin Roof Barn is more than 100 years old and the venue’s website is refreshingly simple with beautiful illustrations. You won’t find any fancy features and virtual tools here, but rather hand-drawn doodles and a stunning hand drawn map of the area.',
     specialPrice: 2050,
+    // specialPrice: 1005,
     totalPrice: 4100,
-    latitude: 32.6428463,
-    longitude: -96.9283827,
-  }){id},
-}
-mutation createVenue2 {
-  createPost(input: {
-    image: "/Users/Michael/repos/storia/assets/images/22.jpg",
-    venue: "Sage Gateshead",
-     description:
-    "Primarily a music venue, Sage Gateshead takes advantage of its central location and large armadillo structure to host events and meetings, as well as shows and concerts. The description of each event space is clearly detailed, complete with virtual tours powered by Google and presentation videos.",
-     specialPrice: 2250,
-     totalPrice: 4500,
-    latitude: 31.7428463,
-    longitude: -96.9283827,
-  }){id},
-}
-mutation createVenue3 {
-  createPost(input: {
-    image: "/Users/Michael/repos/storia/assets/images/23.png",
-    venue: "The Egg",
+    coordinate: {
+      latitude: 32.6428463,
+      longitude: -96.9283827,
+    },
+  },
+  {
+    id: '1',
+    image: '/Users/Michael/repos/storia/assets/images/22.jpg',
+    venue: 'Sage Gateshead',
     description:
-    "The Egg is a 5,000 sqm congress and meeting center located in the heart of Brussels. Their website clearly shows the potential of the venue with lots of pictures showing how versatile the spaces are. The document zone offers essential information event planners should have at hand when planning an event at the Egg,",
-    specialPrice: 2010,
-     totalPrice: 4020,
-    latitude: 33.6428463,
-    longitude: -96.9283827,
-  }){id},
-}
-mutation createVenue4 {
-  createPost(input: {
-    image: "/Users/Michael/repos/storia/assets/images/24.jpg",
-    venue: "Bespoke",
-    description:
-        "Bespoke is a recently established event venue in San Francisco and is, in its own words, “tailored for tech”. Featuring a co-working space, a demo zone for pop-ups and ups and an event venue, the website is minimalist but interactive: the virtual tour of the space is seamless, and options and services are clearly labelled and priced",
+      'Primarily a music venue, Sage Gateshead takes advantage of its central location and large armadillo structure to host events and meetings, as well as shows and concerts. The description of each event space is clearly detailed, complete with virtual tours powered by Google and presentation videos.',
     specialPrice: 2250,
+    //  specialPrice: 1105,
+    totalPrice: 4500,
+    coordinate: {
+      latitude: 31.7428463,
+      longitude: -96.9283827,
+    },
+  },
+  {
+    id: '2',
+    image: '/Users/Michael/repos/storia/assets/images/23.png',
+    venue: 'The Egg',
+    description:
+      'The Egg is a 5,000 sqm congress and meeting center located in the heart of Brussels. Their website clearly shows the potential of the venue with lots of pictures showing how versatile the spaces are. The document zone offers essential information event planners should have at hand when planning an event at the Egg,',
+    specialPrice: 2010,
+    //  specialPrice: 905,
+    totalPrice: 4020,
+    coordinate: {
+      latitude: 33.6428463,
+      longitude: -96.9283827,
+    },
+  },
+  {
+    id: '3',
+    image: '/Users/Michael/repos/storia/assets/images/24.jpg',
+    venue: 'Bespoke',
+    description:
+      'Bespoke is a recently established event venue in San Francisco and is, in its own words, “tailored for tech”. Featuring a co-working space, a demo zone for pop-ups and ups and an event venue, the website is minimalist but interactive: the virtual tour of the space is seamless, and options and services are clearly labelled and priced',
+    specialPrice: 2250,
+    // specialPrice: 1105,
     totalPrice: 5000,
-    latitude: 32.6428463,
-    longitude: -97.9283827,
-  }){id},
-}
-query showAllVenues {
-  listVenues{
-    items {
-      id
-      venue
-      description
-    }
+    coordinate: {
+      latitude: 32.6428463,
+      longitude: -97.9283827,
+    },
+  },
+];
 
- }
-}
+// mutation createVenue1 {
+//   createPost(input: {
+//     image: "/Users/Michael/repos/storia/assets/images/21.jpg",
+//     venue: "The Roof Barn",
+//     description:
+//       "The Tin Roof Barn is more than 100 years old and the venue’s website is refreshingly simple with beautiful illustrations. You won’t find any fancy features and virtual tools here, but rather hand-drawn doodles and a stunning hand drawn map of the area.",
+//     specialPrice: 2050,
+//     totalPrice: 4100,
+//     latitude: 32.6428463,
+//     longitude: -96.9283827,
+//   }){id},
+// }
+// mutation createVenue2 {
+//   createPost(input: {
+//     image: "/Users/Michael/repos/storia/assets/images/22.jpg",
+//     venue: "Sage Gateshead",
+//      description:
+//     "Primarily a music venue, Sage Gateshead takes advantage of its central location and large armadillo structure to host events and meetings, as well as shows and concerts. The description of each event space is clearly detailed, complete with virtual tours powered by Google and presentation videos.",
+//      specialPrice: 2250,
+//      totalPrice: 4500,
+//     latitude: 31.7428463,
+//     longitude: -96.9283827,
+//   }){id},
+// }
+// mutation createVenue3 {
+//   createPost(input: {
+//     image: "/Users/Michael/repos/storia/assets/images/23.png",
+//     venue: "The Egg",
+//     description:
+//     "The Egg is a 5,000 sqm congress and meeting center located in the heart of Brussels. Their website clearly shows the potential of the venue with lots of pictures showing how versatile the spaces are. The document zone offers essential information event planners should have at hand when planning an event at the Egg,",
+//     specialPrice: 2010,
+//      totalPrice: 4020,
+//     latitude: 33.6428463,
+//     longitude: -96.9283827,
+//   }){id},
+// }
+// mutation createVenue4 {
+//   createPost(input: {
+//     image: "/Users/Michael/repos/storia/assets/images/24.jpg",
+//     venue: "Bespoke",
+//     description:
+//         "Bespoke is a recently established event venue in San Francisco and is, in its own words, “tailored for tech”. Featuring a co-working space, a demo zone for pop-ups and ups and an event venue, the website is minimalist but interactive: the virtual tour of the space is seamless, and options and services are clearly labelled and priced",
+//     specialPrice: 2250,
+//     totalPrice: 5000,
+//     latitude: 32.6428463,
+//     longitude: -97.9283827,
+//   }){id},
+// }
+// query showAllVenues {
+//   listVenues{
+//     items {
+//       id
+//       venue
+//       description
+//     }
 
-query showAllVenuesBasedOnLocation {
-  listPosts(filter: {
-    and: {
-      latitude: {
-        between:[
-          32.7254100, 32.7791670],# DFW boundries
-        
-      }
-      longitude: {
-        between: [
-          -97.3208500, -96.8088910
-        ],# DFW boundries
-      }
-    }
-  }) {
-    items {
-      id
-      venue
-      latitude
-      longitude
-    }
-  }
-}
+//  }
+// }
+
+// query showAllVenuesBasedOnLocation {
+//   listPosts(filter: {
+//     and: {
+//       latitude: {
+//         between:[
+//           32.7254100, 32.7791670],# DFW boundries
+
+//       }
+//       longitude: {
+//         between: [
+//           -97.3208500, -96.8088910
+//         ],# DFW boundries
+//       }
+//     }
+//   }) {
+//     items {
+//       id
+//       venue
+//       latitude
+//       longitude
+//     }
+//   }
+// }
